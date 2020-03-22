30b765030b0cf45164472de1a1351e5d1bc8851f
ignacio <ignaciomenchaca666@gmail.com>
2020-03-21 22:24:55 -0600

    Initial commit

---
+++ C-Sharp-Basics/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_Sharp_Basics
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string hello = "World";
+            Console.WriteLine($"Hello {hello}");
+        }
+    }
+}
