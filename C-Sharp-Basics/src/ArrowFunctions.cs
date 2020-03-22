b9efda646e1f67b40e41a8f48b98e2c1876a0070
ignacio <ignaciomenchaca666@gmail.com>
2020-03-22 12:13:20 -0600

    type arrow functions

---
+++ C-Sharp-Basics/src/ArrowFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Sharp_Basics
+{
+    public class ArrowFunctions
+    {
+        private static string Name = "Ignacio";
+        private static int Age = 24;
+        
+        public static void Main(string[] args)
+        {
+            (Name, Age) = Info();
+            Console.WriteLine(Name);
+        }
+        
+        
+        static string Other() => Name; // string
+
+        static (string, int) Info() => (Name, Age); // multi return type data
+    }
+}
--- C-Sharp-Basics/src/Program.cs
+++ C-Sharp-Basics/src/Program.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Linq;
 
 namespace C_Sharp_Basics
 {
     class Program
     {
-        private static string Name = "Ignacio";
-        private static int Age = 24;
+        
 
         public static void Main(string[] args)
         {
             const string world = "World";
-            //Console.WriteLine($"Hello {world}");
-            //Console.WriteLine(Other());
-            //Console.WriteLine(Info());
-            (Name, Age) = Info();
-            Console.WriteLine(Name);
+            Console.WriteLine($"Hello {world}");
         }
 
-        static string Other() => Name; // string
-
-        static (string, int) Info() => (Name, Age); // multi return type data
     }
 }
